a9a93abb41e3f3690163b4bd869e365b4042896d
OscarSanchezM97 <oscarsanchezmeza97@gmail.com>
2020-12-17 12:31:49 -0600

    Create the html, css and js files. Also we are working with copy and paste functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+var h3 = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var newStyle;
+var body = document.querySelector("body");
+
+
+
+    color1.addEventListener('input', setGradient);
+
+    color2.addEventListener('input', setGradient);
+    
+    function setGradient() {
+        newStyle = "linear-gradient(to right,"+color1.value+", "+color2.value+")";
+        h3.innerHTML = "Current style is: background-image: "+newStyle;
+        body.style.backgroundImage = newStyle;
+    }
+    
+
+
+
